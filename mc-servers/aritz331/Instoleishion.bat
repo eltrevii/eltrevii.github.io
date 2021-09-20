a6f1fa78a038ff8833f9623b428d38df02973cb2
Aritz331 <59333567+Aritz331@users.noreply.github.com>
2021-09-20 17:58:08 +0200

    neuw szites ;) and removed mc serveyr ;))

--- mc-servers/aritz331/Instoleishion.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-IF NOT EXIST BuildTools (
-    mkdir BuildTools
-)
-cd BuildTools
-curl -z BuildTools.jar -o BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
-set /p Input=Enter the version: || set Input=latest
-java -jar BuildTools.jar --rev %Input%
-pause

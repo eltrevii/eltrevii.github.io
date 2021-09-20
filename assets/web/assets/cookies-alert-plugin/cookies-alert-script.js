254266f18adde6a73a602f79d0c7b0344a33cf74
Aritz331 <59333567+Aritz331@users.noreply.github.com>
2021-09-20 17:55:48 +0200

    neuw site

--- assets/web/assets/cookies-alert-plugin/cookies-alert-script.js
+++ assets/web/assets/cookies-alert-plugin/cookies-alert-script.js
@@ -1,26 +1,2 @@
-!function(){try{document.getElementsByClassName("engine")[0].getElementsByTagName("a")[0].removeAttribute("rel")}catch(b){}if(!document.getElementById("top-1")){var a=document.createElement("section");a.id="top-1";a.className="engine";a.innerHTML='<a href="https://mobirise.ws">Mobirise Website Builder</a> v4.12.4';document.body.insertBefore(a,document.body.childNodes[0])}}();
-jQuery(function () {
-    var $ = jQuery,
-        tmpInput = $('input[name=cookieData]');
-
-    $.cookiesDirective({
-        customDialogSelector: tmpInput.attr('data-cookie-customDialogSelector') === 'null' ? null : tmpInput.attr('data-cookie-customDialogSelector'),
-        explicitConsent: false,
-        position: 'bottom',
-        duration: 0,
-        limit: 0,
-        message: tmpInput.attr('data-cookie-text'),
-        fontFamily: 'Arial',
-        fontColor: tmpInput.attr('data-cookie-colorText'),
-        fontSize: '13px',
-        backgroundColor: tmpInput.attr('data-cookie-colorBg'),
-        backgroundOpacity: '',
-        linkColor: tmpInput.attr('data-cookie-colorLink'),
-        underlineLink: tmpInput.attr('data-cookie-underlineLink'),
-        textButton: tmpInput.attr('data-cookie-textButton'),
-        colorButton: tmpInput.attr('data-cookie-colorButton'),
-        animate: tmpInput.attr('data-cookie-customDialogSelector') === 'null'
-    });
-
-    tmpInput.remove();
-});
+jQuery(function(){var b=jQuery,a=b("input[name=cookieData]");b.cookiesDirective({customDialogSelector:"null"===a.attr("data-cookie-customDialogSelector")?null:a.attr("data-cookie-customDialogSelector"),explicitConsent:!1,position:"bottom",duration:0,limit:0,message:a.attr("data-cookie-text"),fontFamily:"Arial",fontColor:a.attr("data-cookie-colorText"),fontSize:"13px",backgroundColor:a.attr("data-cookie-colorBg"),backgroundOpacity:"",linkColor:a.attr("data-cookie-colorLink"),underlineLink:a.attr("data-cookie-underlineLink"),
+textButton:a.attr("data-cookie-textButton"),colorButton:a.attr("data-cookie-colorButton"),animate:"null"===a.attr("data-cookie-customDialogSelector")});a.remove()});

e5d82f58d5ee6da05887416af39112e202f8d6e7
trevi <aritz331@gmail.com>
2025-11-11 13:53:37 +0100

    delet and redo everything from scratch

---
+++ astro.config.mjs
@@ -0,0 +1,22 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import sentry from '@sentry/astro';
+import spotlightjs from '@spotlightjs/astro';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import icon from 'astro-icon';
+
+process.env.ASTRO_TELEMETRY_DISABLED = '1';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [sentry({
+    telemetry: false
+  }), spotlightjs(), icon()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});

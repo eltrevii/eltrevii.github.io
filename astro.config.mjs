898c2788466ff684f71e3b859db6269cd15cbebf
trevi <aritz331@gmail.com>
2026-06-20 22:57:14 +0200

    fix: avoid including sentry/spotlight stuff on release

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,37 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import sentry from '@sentry/astro';
-import spotlightjs from '@spotlightjs/astro';
+import sentry from "@sentry/astro";
+import spotlightjs from "@spotlightjs/astro";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import icon from 'astro-icon';
+import icon from "astro-icon";
 
-process.env.ASTRO_TELEMETRY_DISABLED = '1';
+process.env.ASTRO_TELEMETRY_DISABLED = "1";
 
 // https://astro.build/config
 export default defineConfig({
-  trailingSlash: 'always',
-  site: 'https://eltrevii.github.io',
-  integrations: [sentry({
-    telemetry: false
-  }), spotlightjs(), icon()],
+  trailingSlash: "always",
+  site: "https://eltrevii.github.io",
+  integrations: [
+    // enable sentry and spotlight only on development
+    ...(process.env.NODE_ENV === "development"
+      ? [
+          sentry({
+            telemetry: false,
+          }),
+          spotlightjs(),
+        ]
+      : []),
+
+    icon(),
+  ],
 
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
   build: {
-    assets: 'assets'
-  }
+    assets: "assets",
+  },
 });
